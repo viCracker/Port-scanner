0bd999a7876856d1a94672edcb32e90f02bef6ed
Victor Mutiso <mutisovictor69@gmail.com>
2024-05-09 22:30:20 +0300

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -16,7 +16,7 @@ def port_scan(target):
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.settimeout(1)
             result = sock.connect_ex((ip, port))
-            if result == 0:
+            if result == 0: # i.e no error
                 print(f"Port {port} is Open")
             sock.close()
     except socket.gaierror:
